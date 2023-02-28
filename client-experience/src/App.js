29f9d02247a7261acc4cd2bbfe79b493afc69883
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2023-02-28 13:32:12 +0200

    don't forget to comment

--- client-experience/src/App.js
+++ client-experience/src/App.js
@@ -6,16 +6,19 @@ import {
 } from "react-router-dom";
 
 const router = createBrowserRouter([
+  //this path lead to homepage
   {
     path: "/",
     element: <div>Hello world!</div>,
   },
 
+  //this path leads to login page
   {
     path: "/login",
     element: <div>Hellooooooo!</div>,
   },
 
+  //this path leads to registration page
   {
     path: "/register",
     element: <div>yassssssssss!</div>,
