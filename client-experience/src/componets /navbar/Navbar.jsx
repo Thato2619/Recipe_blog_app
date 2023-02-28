77b4c8fd89519f424daeba886c8b63235c1397a3
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2023-02-28 18:46:52 +0200

    add font awesome link index.html
    - style positioning of sections in navbar
    - style navbar using flexbox
    - add icons  (social media info) in the navbar

--- client-experience/src/componets /navbar/Navbar.jsx
+++ client-experience/src/componets /navbar/Navbar.jsx
@@ -6,9 +6,14 @@ import React from 'react'
 export default function Navbar() {
   return (
     <div className='nav'>
-      <div className="topLeft">Left</div>
-      <div className="topCenter">Center</div>
-      <div className="topRight">Right</div>
+      <div className="topLeft">
+      <i class="fa-brands fa-facebook"></i>
+      <i class="fa-brands fa-linkedin"></i>
+      <i class="fa-brands fa-pinterest"></i>
+      <i class="fa-brands fa-instagram"></i>
+      </div>
+      <div className="topCenter">C</div>
+      <div className="topRight">R</div>
     </div>
   )
 }
