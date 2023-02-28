cff6cf2329aadf73d3b143bec243a40d50d89445
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2023-02-28 18:23:14 +0200

    create 3 divs with classNames
    - each classname that reflects different section of the navbar

--- client-experience/src/componets /navbar/Navbar.jsx
+++ client-experience/src/componets /navbar/Navbar.jsx
@@ -6,7 +6,9 @@ import React from 'react'
 export default function Navbar() {
   return (
     <div className='nav'>
-      navbar
+      <div className="topLeft">Left</div>
+      <div className="topCenter">Center</div>
+      <div className="topRight">Right</div>
     </div>
   )
 }
