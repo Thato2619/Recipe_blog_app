e95f725fa46e6c7c42c8c26d19cb5564445591b3
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2023-02-28 22:08:20 +0200

    add all titles and images and icons on navbar

--- client-experience/src/componets /navbar/Navbar.jsx
+++ client-experience/src/componets /navbar/Navbar.jsx
@@ -7,13 +7,29 @@ export default function Navbar() {
   return (
     <div className='nav'>
       <div className="topLeft">
-      <i class="fa-brands fa-facebook"></i>
-      <i class="fa-brands fa-linkedin"></i>
-      <i class="fa-brands fa-pinterest"></i>
-      <i class="fa-brands fa-instagram"></i>
+        <i class="fa-brands fa-pinterest"></i>
+        <i class="fa-brands fa-facebook"></i>
+        <i class="fa-brands fa-instagram"></i>
+        <i class="fa-brands fa-twitter"></i>
+
       </div>
-      <div className="topCenter">C</div>
-      <div className="topRight">R</div>
+      <div className="topCenter">
+        <ul className="navList">
+          <li className="navItems">HOME</li>
+          <li className="navItems">ABOUT</li>
+          <li className="navItems">CONTACT</li>
+          <li className="navItems">WRITE</li>
+          <li className="navItems">LOGOUT</li>
+        </ul>
+      </div>
+      <div className="topRight">
+         <img
+          src=""
+          alt=""
+         />
+
+         <i className="navSearchBar"class="fa-solid fa-magnifying-glass"></i>
+      </div>
     </div>
   )
 }
