54f2e02b19707c6deef84f567fc58959d99415f3
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2023-03-08 17:40:13 +0200

    install react-router-dom and import react-router-dom

--- client-experience/src/App.js
+++ client-experience/src/App.js
@@ -1,7 +1,7 @@
 
 import './App.css';
-import Login
- from './Pages/Login';
+import Login from './Pages/Login';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 function App() {
   return (
     <Login />
