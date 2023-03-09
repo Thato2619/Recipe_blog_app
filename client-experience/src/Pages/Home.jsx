0eb160d3144272b0944a36671419cef61bd3955f
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2023-03-09 10:31:22 +0200

    remove all images in image folder

--- client-experience/src/Pages/Home.jsx
+++ client-experience/src/Pages/Home.jsx
@@ -1,13 +1,88 @@
-import React from 'react'
+import React from "react";
+import sunnyDay from "./images/sunny-day.jpeg";
+import coldDay from "./images/cold-day.jpeg";
+import Description from "./components/description/Descriptions";
+import { useEffect, useState } from "react";
+import { getTheFormattedWeatherData } from "../weatherService";
 
-import {useNavigate} from 'react-router-dom';
+//install react-router dom
 
-const Home = () => {
+
+//page paths that allows me to see the login and registration form
+
+function Home() {
+  
+  //make input text field work
+  const [city, setCity] = useState("Paris");
+  const [weather, setWeather] = useState(null);
+  const [units, setUnits] = useState("metric")
+  const [bg, setbg] = useState(sunnyDay)
+
+  //fetch data
+  useEffect(() => {
+    const fetchWeatherData = async () => {
+      const data = await getTheFormattedWeatherData(city , units);
+      setWeather(data);
+
+      //add dynamic background
+      const thresold = units === "metric" ? 20 : 60 //if tempo is less than 20 or equal to 20°C then change bg to snowy image 
+      if (data.temp <= thresold)  setbg(coldDay);//if tempo is more than 21 or equal to 60°C then change bg to sunny/brighter image 
+      else setbg(sunnyDay)
+    };
+
+    fetchWeatherData();
+  }, [units, city]);
+
+  //add event listner to switch from metric to imperial
+  const handleUnitsClick = (e) => {
+    const button = e.currentTarget;
+    const currentUnit = button.innerText.slice(1);
+
+    const isCelsuis = currentUnit ==="C";
+    button.innerText = isCelsuis ? "°F" : "°C";
+    setUnits(isCelsuis ? "metric" : "imperial") 
+  };
+
+  //create functions that enable text box to work
+  const enterKeyPressed = (e) => {
+    //13 is equilvalent for the 'enter' bucket
+    if (e.keyCode === 13) {
+      setCity(e.currentTarget.value);
+      e.currentTarget.blur()
+    }
+  }
+
   return (
-    <div>
-     
+    //this div is for the bg image
+    <div className="App" style={{ backgroundImage: `url(${bg})` }}>
+      <div className="overlay">
+        {weather && (
+          <div className="container">
+            <div className="section section__inputs">
+              <input onKeyDown={enterKeyPressed} type="text" name="city" placeholder="Enter City" />
+              <button onClick={(e) => handleUnitsClick(e)}>°F</button>
+            </div>
+
+            <div className="section section__temperature">
+              <div className="icon">
+                <h3>{`${weather.name}, ${weather.country}`}</h3>
+                <img src={weather.iconUrl} alt="weather icon"/>
+                <h3> {weather.Description} </h3>
+              </div>
+              <div className="temperature">
+                <h1>{`${weather.temp.toFixed()} ${units === "metric" ? "C" : "F"}` }° </h1>
+              </div>
+            </div>
+            {/*bottom description*/}
+            <Description weather={weather} units={units} />
+          </div>
+        )}
+
+        
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
+
